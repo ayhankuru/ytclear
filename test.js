1d1d26a280dbaf02959046827632acbcbe272595
ayhankuru <ayhankuru@yandex.com.tr>
2015-03-20 01:48:16 +0200

    test added

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ describe('Test!!!...', function(){
 	expect(ytclear("Adventure Club - Retro City (LOUDPVCK Remix) [FREE DL]")).to.eql("Adventure Club - Retro City");
 	expect(ytclear("Deadmau5 - Strobe (Lemony's Trap Remix) 2012")).to.eql("Deadmau5 - Strobe");
 	expect(ytclear("Skrillex - Dirty Vibe with Diplo, CL, & G-Dragon (OFFICIAL VIDEO)")).to.eql("Skrillex - Dirty Vibe with Diplo, CL, & G-Dragon");
-		
+	expect(ytclear("Jack U - Live at Madison Square Garden, NY - 1/1/15 (With Video)(FULL SET)")).to.eql("Jack U - Live at Madison Square Garden, NY - 1/1/15")
 	done();
 			
 
